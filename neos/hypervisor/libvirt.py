ff4a43a7ee1f1e39ec6fa53ea8a6ef88810ad1f4
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2017-04-05 03:26:25 -0400

    Added hypervisor libvirt template

---
+++ neos/hypervisor/libvirt.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+# vim:sw=4:ts=4:et:
+"""Libvirt Hypervisor file."""
+from neos.hypervisor import NeosHypervisor
+
+
+class NeosHypervisorLibvirt(NeosHypervisor):
+    """Define generic class for Libvirt Neos Hypervisor."""
+    pass
